670a2ac72619c7e95aa4627f7961f351e9a5145a
rakeshkumarnahak <rakeshkumarnahak1@gmail.com>
2024-08-14 19:58:27 +0530

    Routes are defined

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,22 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes";
+import cors from "cors";
 
+const app = express();
+app.use(cors());
 
+dotenv.config();
+const port = process.env.PORT;
+
+const rootRouter = router;
+
+app.get("/", (req, res) => {
+  res.send("Hello Fucking world");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.use("/api/v1", rootRouter);
