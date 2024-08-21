1bfe6a74568d30d4e82bd65908483e872f87573f
Subrajeet Maharana <subrajeet.info@gmail.com>
2024-08-21 22:09:53 +0530

    added Send, Signin and Signup components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button';
+import {Signup} from './components/custom/Signup'
 import {Signin} from './components/custom/Signin'
+import {Send} from './components/custom/Send'
 
 function App() {
  const [count, setCount] = useState(0)
  return (
    <>
-     <Button>Secondary</Button> <br /><br />
-     <Button variant="destructive">Here</Button> <br /><br />
-     <Button variant="secondary">Secondary</Button> <br /><br />
-     <Button variant="ghost">Secondary</Button> <br /><br />
-    <Signin/>
+    <Send/>
    </>
  )
 }
