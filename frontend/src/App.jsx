7108e3872193d2e379ee8a01f8c31cded6e2a80a
Subrajeet Maharana <subrajeet.info@gmail.com>
2024-08-21 18:36:49 +0530

    added shadcn ui lib to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button';
 
 function App() {
-
-  return (
-    <div>
-        Hello world
-    </div>
-  )
+ const [count, setCount] = useState(0)
+ return (
+   <>
+     <Button>Secondary</Button> <br /><br />
+     <Button variant="destructive">Here</Button> <br /><br />
+     <Button variant="secondary">Secondary</Button> <br /><br />
+     <Button variant="ghost">Secondary</Button> <br /><br />
+   </>
+ )
 }
 
 export default App
+
